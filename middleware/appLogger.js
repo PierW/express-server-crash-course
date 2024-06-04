882a4bcc801f3f8d7bf922c954239b3aff32f93c
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2024-06-04 18:22:55 +0200

    aggiunti colori per messaggi in console

--- middleware/appLogger.js
+++ middleware/appLogger.js
@@ -1,6 +1,16 @@
+import colors from 'colors';
+
 // Middleware Route Level
 const appLogger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} APP LEVEL`);
+    const methodColors = {
+        GET : 'green',
+        PUT : 'yellow',
+        POST: 'blue',
+        DELETE: 'red'
+    };
+    const color = methodColors[req.method] || 'white';
+
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} APP LEVEL`[color]);
     next();
 }
 
